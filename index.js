5b338c2f7849edde74484c275da2c29254864aa3
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2024-01-20 23:31:36 +0200

    done!

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-let button = document.querySelector("button");
-
-button.addEventListener("click", function () {
-
+function rollDiceAndDetermineWinner() {
     //for dice 1
     var randNum1 = Math.floor(Math.random() * 6) + 1; //generating number from 1-6
     var randDiceImg = "dice" + randNum1 + ".png";  // generating dice images i.e dice1.png - dice6.png
@@ -14,9 +11,15 @@ button.addEventListener("click", function () {
     var randImgSrc2 = "images/dice" + randNum2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", randImgSrc2);
 
+    if (randNum1 > randNum2) {
+        document.querySelector("h1").innerHTML = "🚩 Player 1 wins!"
+    } else if (randNum2 > randNum1) {
+        document.querySelector("h1").innerHTML = "🚩 Player 2 wins!!"
+    } else {
+        document.querySelector("h1").innerHTML = "Draw!"
+    }
+}
 
-
-
-
-
-});
+// Call the function when the button is clicked
+let button = document.querySelector("button");
+button.addEventListener("click", rollDiceAndDetermineWinner);
